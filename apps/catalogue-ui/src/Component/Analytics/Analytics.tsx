6e5c039700570b7bdaec0bc4a0df62f0986ceba6
CL-GeorgeMathewPaul <george.paul@cirruslabs.io>
2024-06-26 13:59:04 +0530

    Added the filter dropdown. Removed some unnecessary css

--- apps/catalogue-ui/src/Component/Analytics/Analytics.tsx
+++ apps/catalogue-ui/src/Component/Analytics/Analytics.tsx
@@ -1,20 +1,15 @@
-import filter from '../../app/assets/filter.png';
-import downarr from '../../app/assets/arrow-down.png';
 import './Analytics.css'
 import StickyHeadTable from './Table';
 import BarGraph from './BarGraph';
 import BasicPie from './PieChart';
+import FilterDropdown from '../FilterDropdown/filter';
 
 export default function Analytics() {
   return (
     <div className="analytics-container">
         <h1 className="analytics_header">Analytics</h1>
-        <div className="header_align_home">
-          <div className="filter_align">
-            <img src={filter} alt="Logo" className="h-5 m-1" />
-            <span>Filter</span>
-            <img src={downarr} alt="Logo" className="h-5 m-1 ml-4" />
-          </div>
+        <div className="ml-8 mt-4 mb-4 ">
+          <FilterDropdown />
         </div>
 
       <div className="grid grid-cols-1 ml-8 md:grid-cols-2 gap-4">
