52548ed41383df3bc3f37b8db9719600ba76c4f4
CL-Arjun-Saini <arjun.saini@cirruslabs.io>
2024-07-03 14:01:15 -0500

    Feat/docker (#11)
    * Add backend Dockerfile
    
    * Add frontend Dockerfile
    
    * Add docker compose

--- apps/catalogue-ui/vite.config.ts
+++ apps/catalogue-ui/vite.config.ts
@@ -1,35 +1,40 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
-export default defineConfig({
-  root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/catalogue-ui',
+export default defineConfig( ({mode}) => 
+{
+  const env = loadEnv(mode, process.cwd(), '');
 
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
+  return {
+    root: __dirname,
+    cacheDir: '../../node_modules/.vite/apps/catalogue-ui',
 
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-
-  plugins: [react(), nxViteTsPaths()],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
-  build: {
-    outDir: '../../dist/apps/catalogue-ui',
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
+    server: {
+      port: 4200,
+      host: env.HOST,
     },
-  },
+
+    preview: {
+      port: 4300,
+      host: 'localhost',
+    },
+
+    plugins: [react(), nxViteTsPaths()],
+
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+
+    build: {
+      outDir: '../../dist/apps/catalogue-ui',
+      emptyOutDir: true,
+      reportCompressedSize: true,
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
+    },
+  }
 });
