c7ed120be5a5bbf141e9978be7f19467a1032e4e
Arjun Saini <arjun.saini@cirruslabs.io>
2024-07-11 12:40:59 -0500

    Add dashboard module

--- apps/catalogue-backend/src/app/app.module.ts
+++ apps/catalogue-backend/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from '../users/users.module';
+import { DashboardModule } from '../dashboard/dashboard.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_URI), ProjectsModule, AuthModule, UsersModule],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URI), ProjectsModule, AuthModule, UsersModule, DashboardModule],
   controllers: [AppController],
   providers: [AppService],
 })
