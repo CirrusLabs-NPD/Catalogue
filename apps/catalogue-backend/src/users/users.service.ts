38bf9f056c7fb3f59ea7eb980e5be20df7618c22
Arjun Saini <arjun.saini@cirruslabs.io>
2024-07-11 10:59:33 -0500

    Delete comments

--- apps/catalogue-backend/src/users/users.service.ts
+++ apps/catalogue-backend/src/users/users.service.ts
@@ -6,24 +6,8 @@ import { Model } from 'mongoose';
 @Injectable()
 export class UsersService {
     constructor(@InjectModel(UserClass.name) private userModel: Model<UserClass>) {}
-
-    // private readonly users = [
-    //     {
-    //         email: "arjun.saini@cirruslabs.io"
-    //     },
-    //     {
-    //         email: "braden.johnson@cirruslabs.io"
-    //     },
-    //     {
-    //         email: "farhan.soomro@cirruslabs.io"
-    //     },
-    //     {
-    //         email: "rohan.shah@cirruslabs.io"
-    //     },
-    // ]
-
+    
     async findUser(email: string) {
         return await this.userModel.findOne({ email: email }).exec();
-        // return this.users.find(user => user.email === email);
     }
 }
