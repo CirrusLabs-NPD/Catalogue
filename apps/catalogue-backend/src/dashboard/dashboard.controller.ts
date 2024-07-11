8fe3cf73096b04f11300decef9ed6cee20207ec3
Braden Johnson <bradenj@seas.upenn.edu>
2024-07-11 14:08:45 -0400

    swagger format

--- apps/catalogue-backend/src/dashboard/dashboard.controller.ts
+++ apps/catalogue-backend/src/dashboard/dashboard.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
+import { ApiTags, ApiResponse, ApiParam, ApiBody } from '@nestjs/swagger';
 
+@ApiTags('dashboard')
 @Controller('dashboard')
 export class DashboardController {
     constructor(private readonly dashboardService: DashboardService) {}
@@ -11,6 +13,7 @@ export class DashboardController {
     }
 
     @Get('percentDash')
+    @ApiResponse({ status: 200, description: 'Returns name, duration, status and percentage for all projects.' })
     getPercentDash() {
         return this.dashboardService.getPercentDash();
     }
