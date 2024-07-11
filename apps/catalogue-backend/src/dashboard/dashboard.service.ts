364f86201d8499da0ae25f0bfa89036b0f6e886b
Arjun Saini <arjun.saini@cirruslabs.io>
2024-07-11 12:56:27 -0500

    Link database to dashboard module

--- apps/catalogue-backend/src/dashboard/dashboard.service.ts
+++ apps/catalogue-backend/src/dashboard/dashboard.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { ProjectClass } from '../projects/schemas/project.schemas';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class DashboardService {}
+export class DashboardService {
+    constructor(@InjectModel(ProjectClass.name) private projectModel: Model<ProjectClass>) {}
+
+    async getMonthlyCompletion() {
+        return this.projectModel.find().exec();
+    }
+}
