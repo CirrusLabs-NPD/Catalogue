a011edc61ed6a2bf7a5798b2c798b12d8f830b34
CL-Arjun-Saini <arjun.saini@cirruslabs.io>
2024-07-17 14:48:49 -0500

    Add example fields in Swagger UI (#18)

--- apps/catalogue-backend/src/projects/dto/update.project.dto.ts
+++ apps/catalogue-backend/src/projects/dto/update.project.dto.ts
@@ -1,26 +1,31 @@
 import { IsNotEmpty, IsString, IsUrl, IsArray, IsNumber, Min, Max, IsOptional, IsDateString, IsEnum } from "class-validator";
 import { ProjectStatus } from "../schemas/project-status.enum";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class UpdateProjectDto {
     @IsString()
     @IsNotEmpty()
     @IsOptional()
+    @ApiProperty({ example: 'Example Project', description: 'Name of project', required: false })
     projectName?: string;
 
     @IsString()
     @IsNotEmpty()
     @IsOptional()
+    @ApiProperty({ example: '8 weeks', description: 'Time duration of project', required: false })
     duration?: string;
 
     @IsUrl()
     @IsNotEmpty()
     @IsOptional()
+    @ApiProperty({ example: 'https://github.com/CirrusLabs-NPD/example-repo', description: 'Link to project repo', required: false })
     gitHubLinks?: string;
 
     @IsArray()
     @IsString({ each: true })
     @IsNotEmpty({ each: true })
     @IsOptional()
+    @ApiProperty({ example: ["Technology 1", "Technology 2"], description: 'Technologies used in project', required: false })
     technology?: string[];
 
     /* @IsArray()
@@ -33,17 +38,20 @@ export class UpdateProjectDto {
     @IsNotEmpty()
     @IsEnum(ProjectStatus)
     @IsOptional()
+    @ApiProperty({ example: 'Ongoing', description: 'Status of project', required: false })
     projectStatus?: string;
 
     @IsArray()
     @IsString({ each: true })
     @IsNotEmpty({ each: true })
     @IsOptional()
+    @ApiProperty({ example: ["Member 1", "Member 2"], description: 'Members involved in project', required: false })
     members?: string[];
 
     @IsString()
     @IsNotEmpty()
     @IsOptional()
+    @ApiProperty({ example: "Example description", description: 'Description of project', required: false })
     description?: string;
 
     @IsNumber()
@@ -51,9 +59,11 @@ export class UpdateProjectDto {
     @Max(100)
     @IsNotEmpty()
     @IsOptional()
+    @ApiProperty({ example: 50, description: 'Percent completion of project', required: false })
     progressPercent?: number;
 
     @IsDateString()
     @IsOptional()
+    @ApiProperty({ example: '2020-01-31', description: 'Completion date of project in YYYY-MM-DD format', required: false })
     completionDate: string;
 }
