ae2e88d79354dd8c3f6f0d05846d5ee6c62aa3a6
Braden Johnson <bradenj@seas.upenn.edu>
2024-07-22 17:46:34 -0400

    swagger formatting for project status

--- apps/catalogue-backend/src/statuses/dto/update-status.dto.ts
+++ apps/catalogue-backend/src/statuses/dto/update-status.dto.ts
@@ -1,8 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsOptional, IsString} from "class-validator";
 
 export class UpdateStatusDto {
     @IsString()
     @IsNotEmpty()
     @IsOptional()
+    @ApiProperty({ example: 'Example Status', description: 'Name of status', required: false })
     projectStatus?: string;
 }
