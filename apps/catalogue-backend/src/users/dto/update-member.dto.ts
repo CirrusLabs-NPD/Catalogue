22e4c48c0935029efd7b332cb12d13b7a6c5fdac
Braden Johnson <bradenj@seas.upenn.edu>
2024-08-08 16:35:34 -0400

    added project manager & member schema/dtos

---
+++ apps/catalogue-backend/src/users/dto/update-member.dto.ts
@@ -0,0 +1,36 @@
+import { IsNotEmpty, IsString, IsUrl, IsArray, IsNumber, Min, Max, IsOptional, IsDateString, IsEnum } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateMemberDto {
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
+    @ApiProperty({ example: 'John Doe', description: 'Name of team member', required: false })
+    name?: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
+    @ApiProperty({ example: 'john.doe@cirruslabs.io', description: 'Email of team member', required: false })
+    email?: string;
+
+    @IsUrl()
+    @IsNotEmpty()
+    @IsOptional()
+    @ApiProperty({ example: 'Backend Engineer', description: 'Job title/role of team member', required: false })
+    title?: string;
+
+    @IsArray()
+    @IsString({ each: true })
+    @IsNotEmpty({ each: true })
+    @IsOptional()
+    @ApiProperty({ example: ["Technology 1", "Technology 2"], description: 'Technology stack for team member', required: false })
+    techStack?: string[];
+
+    @IsArray()
+    @IsString({ each: true })
+    @IsNotEmpty({ each: true })
+    @IsOptional()
+    @ApiProperty({ example: ["Project 1", "Project 2"], description: 'Projects team member has worked on.', required: false })
+    projects?: string[];
+}
