c85b1e35e22fd8eef3400a23c8dcdaab57261d03
Braden Johnson <bradenj@seas.upenn.edu>
2024-08-15 17:19:25 -0400

    home page scroll fix?

--- apps/catalogue-ui/src/Component/Home.tsx
+++ apps/catalogue-ui/src/Component/Home.tsx
@@ -50,8 +50,8 @@ const Home: React.FC = () => {
   }
 
   return (
-    <div className="ml-64 mt-6 h-full overflow-y-scroll">
-      <div className="overflow-y-auto h-full">
+    <div className="ml-64 mt-6 max-h-[calc(100vh-100px)] overflow-y-auto">
+      <div className="h-full">
         <h1 className="text-[#2C4B84] text-4xl pl-8 pt-1">Project Catalogue</h1>
         <div className="flex space-x-4 mt-5 mb-4">
           <FilterDropdown />
